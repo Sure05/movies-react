46d620628572341df2a8438ed003407f8678418d
Karpovich Vladislav <deurlex@gmail.com>
2022-08-26 13:26:30 +0300

    add pagination

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/movies-react/">
             <Route index element={<Home />} />
-            <Route path="/:id" element={<Details />}>
+            <Route path="/movies-react/:id" element={<Details />}>
             </Route>
           </Route>
         </Routes>
